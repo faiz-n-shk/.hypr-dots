ac779058be01b535b0b9481ccadb9a8f7f7697b3
Faizan Shaikh <shaikhfaizan.dev@gmail.com>
2026-07-19 20:54:09 +0530

    remove non working reset db keybind in clipboard script

--- dot_config/hypr/colors/colors-matugen.lua
+++ dot_config/hypr/colors/colors-matugen.lua
@@ -1,7 +1,7 @@
 local M = {
-	primary = "rgba(81d3dfee)",
-	secondary = "rgba(b1cbcfee)",
-	inactive = "rgba(303637aa)",
+	primary = "rgba(f1be6dee)",
+	secondary = "rgba(dcc3a1ee)",
+	inactive = "rgba(3a342baa)",
 }
 
 return M
